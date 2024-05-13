14c5c0814ff66555899a178c44f704def3e0d555
Owen Dolan <odolan27@gmail.com>
2024-05-12 23:28:22 -0400

    updates

---
+++ main.py
@@ -0,0 +1,99 @@
+import functions_framework
+from flask import jsonify
+from firebase_admin import credentials, firestore, initialize_app
+from packages import util
+from twilio.rest import Client
+import yaml
+import os
+
+def load_yaml_file(filepath):
+    with open(filepath, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+# Use the function to load the configuration
+config = load_yaml_file('config.yaml')
+
+env = os.getenv("deployment_env")
+
+TWILIO_ACCOUNT_SID = config[env]["twilio"]["account_sid"] 
+TWILIO_AUTH_TOKEN = config[env]["twilio"]["auth_token"] 
+
+# Initialize Firebase Admin SDK with the service account key
+cred = credentials.Certificate("firebase_creds.json")  # Update with your service account key file 
+initialize_app(cred)
+db = firestore.client() # set firestore client
+
+twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+"""
+{
+    "user_session_token": "12345abcde",
+    "phone_number": "+12032248444",
+    "user_location": {
+        lat: 0.00,
+        lon: 0.00,
+    },
+    "prescription": {
+        "name": "Focalin",
+        "dosage": "10",
+        "brand_or_generic": "Generic",
+        "quantity": "30",
+        "type": "Extended%20Release"
+    }
+}
+"""
+@functions_framework.http
+def main(request):
+    
+    # Set CORS headers for the preflight request
+    if request.method == "OPTIONS":
+        headers = {
+            "Access-Control-Allow-Origin": "*", # change from "*" to "https://rx-radar.com" for production
+            "Access-Control-Allow-Methods": "POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Max-Age": "3600",
+        }
+        return ("", 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {"Access-Control-Allow-Origin": "*"} # change from "*" to "https://rx-radar.com" for production
+
+
+        
+    # Get the JSON data from the request
+    request_data = request.get_json(silent=True)
+
+    # If the token is valid, proceed with the request processing
+    success, out, code = util.validate_request(request_data)
+    if not success:
+        return out, code, headers
+
+    # verify the user session token
+    user_session_token = request_data["user_session_token"]
+    verification_token = util.verify_user_token(token=user_session_token)
+    if not verification_token:
+        # If the user session token is incorrect, return a 401 Unauthorized response
+        return jsonify({'error': 'Unauthorized'}), 401, headers
+
+    phone_number = request_data["phone_number"]
+
+    # checks that the user is valid to place calls 
+    user_can_search, user_uuid, err = util.can_user_search(db, phone_number)
+    if not user_can_search: 
+        return err, headers
+    
+    if util.get_user_search_credit(user_uuid=user_uuid) > 0:
+        # Push new search to search_requests
+        util.db_add_search(db, request_data, user_uuid, 'SEARCH_REQUESTS')
+        return jsonify({'message': 'Request is valid dont show payment'}), 200, headers
+
+    # Push new search to pending_search_requests
+    util.db_add_search(db, request_data, user_uuid, 'PENDING_SEARCH_REQUESTS')
+
+    return jsonify({'message': 'Request is valid show payment'}), 200, headers
+
+
+
+
+        
---
+++ packages/util.py
@@ -0,0 +1,219 @@
+from firebase_admin import auth, firestore
+from google.protobuf import timestamp_pb2
+from google.cloud import tasks_v2
+from twilio.rest import Client
+from flask import jsonify
+import requests
+import datetime
+import time
+import uuid
+import json
+import yaml
+import os
+
+
+def load_yaml_file(filepath):
+    with open(filepath, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
+# Use the function to load the configuration
+config = load_yaml_file('config.yaml')
+
+env = os.getenv("deployment_env")
+
+TWILIO_ACCOUNT_SID = config[env]["twilio"]["account_sid"] 
+TWILIO_AUTH_TOKEN = config[env]["twilio"]["auth_token"] 
+TWILIO_PHONE_NUMBER = config[env]["twilio"]["phone_number"] 
+
+FIREBASE_USERS_DB = config[env]["firebase"]["users_db"]
+FIREBASE_CALLS_DB = config[env]["firebase"]["calls_db"]
+FIREBASE_SEARCH_REQUESTS_DB = config[env]["firebase"]["search_requests_db"]
+FIREBASE_PENDING_SEARCH_REQUESTS_DB = config[env]["firebase"]["pending_search_requests_db"]
+
+CF_GET_PHARMACIES = config[env]["cloud_functions"]["get_pharmacies"]
+CF_CREATE_CALL = config[env]["cloud_functions"]["create_call"]
+
+
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+
+# checks if user is within search request limit today, create new user if non exists
+def can_user_search(db, phone_number):
+    try: 
+        # query for existing user
+        matching_users_query_ref = db.collection(FIREBASE_USERS_DB).where('phone', '==', phone_number).limit(1)
+        matching_user_docs = matching_users_query_ref.get()
+
+        # if the user doc does not exist, create new user and return true
+        if len(matching_user_docs) == 0:
+            new_user_uuid = str(uuid.uuid4())
+
+            new_user_doc = {
+                "phone": phone_number,
+                "user_uuid": new_user_uuid
+            }
+            
+            db.collection(FIREBASE_USERS_DB).document(new_user_uuid).set(new_user_doc)
+            return True, new_user_uuid
+    
+        user_doc = matching_user_docs[0].to_dict()
+
+        # get existing user information
+        last_search_timestamp = user_doc.get("last_search_timestamp")
+        user_uuid = user_doc.get("user_uuid")
+
+        # if user should be rate limited
+        SECONDS_IN_DAY = 86400
+        if (time.time() - last_search_timestamp) < SECONDS_IN_DAY:
+            return False, user_uuid, (jsonify({"error": "User searched too many times"}), 400)
+        
+        # if user should not be rate limited
+        return True, user_uuid, None
+
+    except Exception as e:
+        return False, None, (jsonify({"error": "Internal error occured: failed to check if user can search"}), 500)
+
+# send sms message
+def send_sms(twilio_client, phone_number, msg):
+  try: 
+    new_message = twilio_client.messages.create(to=phone_number, from_= TWILIO_PHONE_NUMBER, body=msg)
+  except Exception as e: 
+    return jsonify({"error": f"Internal error occured: send_sms {e}"})
+  
+
+# returns the number of search credits a user has available
+def get_user_search_credit(db, user_uuid):
+    user_doc_ref = db.collection(FIREBASE_USERS_DB).document(user_uuid)
+
+    # Fetch the user document
+    user_doc = user_doc_ref.get()
+
+    # Check if the document exists
+    if user_doc.exists:
+        search_credits = user_doc.to_dict().get('search_credits')
+        return search_credits
+
+
+# creates a new search request
+def db_add_search(db, req_obj, user_uuid, db_location):
+    # Generate a unique ID for the document
+    search_request_uuid = str(uuid.uuid4())
+
+    # Current epoch time
+    epoch_initiated = int(time.time())
+
+    # prescription object
+    user_location = req_obj["user_location"]
+    med_name = req_obj["prescription"]["name"] 
+    med_dosage = req_obj["prescription"]["dosage"]
+    med_brand = req_obj["prescription"]["brand_or_generic"]
+    med_quantity = req_obj["prescription"]["quantity"]
+    med_type = req_obj["prescription"]["type"]
+
+    # Data to be added
+    data = {
+        "search_request_uuid": search_request_uuid,
+        "user_uuid": user_uuid,
+        "user_location": user_location,
+        "prescription": {
+            "name": med_name,
+            "dosage": med_dosage,
+            "brand": med_brand,
+            "quantity": med_quantity,
+            "type": med_type
+        },
+        "contains_fillable": False,
+        "calls_remaining": 0,
+        "calls" : [],
+        "epoch_initiated": epoch_initiated,
+    }
+
+    # Add the data to a new document in the 'medications' collection
+    if db_location == 'PENDING_SEARCH_REQUESTS':
+        db.collection(FIREBASE_PENDING_SEARCH_REQUESTS_DB).document(search_request_uuid).set(data)
+    else:
+        db.collection(FIREBASE_SEARCH_REQUESTS_DB).document(search_request_uuid).set(data)
+
+
+# verifies user session token
+def verify_user_token(token):
+    try:
+        decoded_token = auth.verify_id_token(token)
+        uid = decoded_token['uid']
+        # The token is valid
+        return uid
+    except auth.InvalidIdTokenError:
+        # The token is invalid
+        return None
+
+
+# validates user medication request body
+def validate_request(request_data):
+    required_fields = ['user_session_token', 'phone_number', 'user_location', 'prescription'] # required fields
+    prescription_fields = ['name', 'dosage', 'brand_or_generic', 'quantity', 'type'] # required fields within medication
+    location_fields = ['lat', 'lon'] # required fields within user_location
+
+    # Check if all required fields exist
+    for field in required_fields:
+        if field not in request_data:
+            return False, jsonify({'error': f'Missing required field: {field}'}), 400
+
+    # Check if the types are correct
+    if not isinstance(request_data.get('user_session_token'), str):
+        return False, jsonify({'error': 'user_session_token must be a string'}), 400
+
+    if not isinstance(request_data.get('phone_number'), str):
+        return False, jsonify({'error': 'phone_number must be a string'}), 400
+
+    ## ------------------------------------------- ##
+    # Check Location fields and types
+
+    user_location = request_data.get('user_location')
+
+    # check if location is empty
+    if not user_location:
+        return False, jsonify({'error': 'user_location object cannot be empty'}), 400
+    
+    # check that all the prescription fields exist
+    for field in location_fields:
+        if field not in user_location:
+            return False, jsonify({'error': f'Missing required field inside user_location: {field}'}), 400
+    
+    if not isinstance(user_location.get('lat'), float):
+        return False, jsonify({'error': 'user_location lat must be a float'}), 400
+    if not isinstance(user_location.get('lon'), float):
+        return False, jsonify({'error': 'user_location lon must be a float'}), 400
+    
+    # make sure the lat and lon values are valid
+    if user_location.get('lat') < -90 or user_location.get('lat') > 90 or user_location.get('lon') < -180 or user_location.get('lon') > 180: 
+        return False, jsonify({'error': 'user_location lat and lon must be valid'}), 400
+
+    ## ------------------------------------------- ##
+    # Check prescription fields and types
+
+    prescription = request_data.get('prescription')
+
+    # check if prescription is empty 
+    if not prescription:
+        return False, jsonify({'error': 'prescription object cannot be empty'}), 400
+
+    # check that all the prescription fields exist
+    for field in prescription_fields:
+        if field not in prescription:
+            return False, jsonify({'error': f'Missing required field inside prescription: {field}'}), 400
+
+    # check that prescription object field types are valid
+    if not isinstance(prescription.get('name'), str):
+        return False, jsonify({'error': 'prescription name must be a string'}), 400
+    if not isinstance(prescription.get('dosage'), str):
+        return False, jsonify({'error': 'prescription dosage must be a string'}), 400
+    if not isinstance(prescription.get('brand_or_generic'), str):
+        return False, jsonify({'error': 'prescription brand_or_generic must be a string'}), 400
+    if not isinstance(prescription.get('quantity'), str):
+        return False, jsonify({'error': 'prescription quantity must be a string'}), 400
+    if not isinstance(prescription.get('type'), str):
+        return False, jsonify({'error': 'prescription type must be a string'}), 400
+
+    # on valid
+    return True, None, 200
